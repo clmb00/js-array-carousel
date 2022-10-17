736c2518913df9348a20510f9b8ecb6e8c72c37c
clmb00 <paolo.colombo2000@gmail.com>
2022-10-17 15:07:03 +0200

    [ADD] Bonus 1

---
+++ Bonus_1/script.js
@@ -0,0 +1,25 @@
+imgList = [
+  "01.jpg", 
+  "02.jpg", 
+  "03.jpg", 
+  "04.jpg", 
+  "05.jpg", 
+];
+let counter = 0;
+
+document.getElementById("up").addEventListener('click', function(){
+  counter --;
+  if (counter<0){
+    counter = imgList.length - 1;
+  };
+  document.querySelector('#main_img img').src = "../img/" + imgList[counter];
+});
+
+document.getElementById("down").addEventListener('click', function(){
+  counter ++;
+  if (counter>imgList.length - 1){
+    counter = 0;
+  };
+  document.querySelector('#main_img img').src = "../img/" + imgList[counter];
+});
+
