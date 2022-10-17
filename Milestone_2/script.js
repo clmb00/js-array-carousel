53e3c2cb6dc20a323e34c55363b3b61752c4de20
clmb00 <paolo.colombo2000@gmail.com>
2022-10-17 14:52:10 +0200

    [ADD] Milestone 2

---
+++ Milestone_2/script.js
@@ -0,0 +1,12 @@
+imgList = [
+  "01.jpg", 
+  "02.jpg", 
+  "03.jpg", 
+  "04.jpg", 
+  "05.jpg", 
+];
+
+//E' inutile lo so
+for(i=0;i<imgList.length;i++){
+  document.querySelector('#main_img img').src = "../img/" + imgList[i];
+};
